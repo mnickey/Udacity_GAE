2363b483d1602cdbf137a664dec55c59f5fa2a7f
Michael Nickey <mnickey@gmail.com>
2015-07-14 23:16:19 -0700

    added sendmail cron job

--- wired-tea-95020/main.py
+++ wired-tea-95020/main.py
@@ -4,13 +4,26 @@ from google.appengine.api import app_identity
 from google.appengine.api import mail
 from conference import ConferenceApi
 
+
 class SetAnnouncementHandler(webapp2.RequestHandler):
     def get(self):
         """Set Announcement in Memcache."""
-        # TODO 1
-        # use _cacheAnnouncement() to set announcement in Memcache
         ConferenceApi._cacheAnnouncement()
+
+
+class SendConfirmationEmailHandler(webapp2.RequestHandler):
+    def post(self):
+        """ Send email confirming Conference Creation. """
+        mail.send_mail(
+            'noreply@%s.appspotmail.com' % (
+                app_identity.get_application_id()),
+            self.request.get('email'),
+            'You created a new Conference!',
+            'Hi, you have created the following conference: \r\n\r\n%s' % self.request.get(
+                'conferenceInfo')
+        )
 
 app = webapp2.WSGIApplication([
     ('/crons/set_announcement', SetAnnouncementHandler),
-], debug=True)
+    ('/tasks/send_confirmation_email', SendConfirmationEmailHandler)
+    ], debug=True)
